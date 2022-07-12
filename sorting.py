587327d3769b915ddf6d8136bd472918b0f43d57
Mihir Gupta <69476837+gupta-mihir@users.noreply.github.com>
2022-07-12 16:42:11 -0400

    First commit

---
+++ sorting.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+msg = "This is a sorting program. We will compare all the popular sorting algorithms to see which is the best."
+print(msg)
+print("This is when the sorting program starts")
+class Sort:
+
+    def bin_sort(self, nums: List[int]) -> List[int] :
+        #do stuff BINARY SORT
+        print("Binary Sort")
+
+    def bubble(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Bubble Sort")
+
+    def bucket(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Bucket Sort")
+
+    def counting(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Counting Sort")
+        
+    def heap(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Heap Sort")
+
+    def insertion(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Insertion Sort")
+
+    def merge(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Merge Sort")
+
+    def quick(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Quick Sort")
+
+    def radix(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Radix Sort")
+
+    def selection(self, nums: List[int]) -> List[int] :
+        #do stuff bubble sort 
+        print("Selection Sort")
